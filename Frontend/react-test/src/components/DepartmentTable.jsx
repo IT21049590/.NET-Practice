66cbf7faf099fb146e260fc3a9b0aedbfc270eef
Hiruna Lalanga <IT21049590@my.sliit.lk>
2024-11-01 18:27:03 +0530

    fixed toast issue and refactor code

--- Frontend/react-test/src/components/DepartmentTable.jsx
+++ Frontend/react-test/src/components/DepartmentTable.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import api from '../../api'
 import DepartmentModal from './DepartmentModal'
+import { toast } from 'react-toastify'
 import './DepartmentTable.css'
 
 function DepartmentTable() {
